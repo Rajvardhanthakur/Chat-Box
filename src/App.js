b8a368b4d88c0de2424383c2505c6d7ee754bbbf
Rajvardhan <rajthakur3619@gmail.com>
2020-07-16 19:03:04 +0530

    component created for messages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {Button, FormControl, InputLabel, Input} from "@material-ui/core";
+import Message from './Message';
 
 function App() {
 
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState(["hello, hii, whats'up"]);
+  const [username, setUsername] = useState('');
 
-  console.log(input)
+
+  useEffect(() => {
+    setUsername(prompt('please enter your name!'))
+  }, [])
+
 
   const sendMessage = (event) => {
     event.preventDefault();
     setMessages([...messages, input]);
     setInput('');
   }
+
+
   
   return (
     <div className="App">
       <h1>Messenger Clone</h1>
       
       <form>
-        <input value={input} onChange={event => setInput(event.target.value)} />
-        <button type='submit' onClick={sendMessage}>Send Message</button>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+          <Button disabled={!input} varient="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+        
       </form>
 
       {
         messages.map(message => (
-          <p>{message}</p>
+          <Message text={message} />
         ))
       }
     </div>
