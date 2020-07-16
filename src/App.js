aac4bd9c5148148d2be36178911140606f87a667
Rajvardhan <rajthakur3619@gmail.com>
2020-07-16 18:05:10 +0530

    Starting point

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(["hello, hii, whats'up"]);
+
+  console.log(input)
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  }
+  
   return (
     <div className="App">
-      <h1>Hello everyone</h1>
+      <h1>Messenger Clone</h1>
+      
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type='submit' onClick={sendMessage}>Send Message</button>
+      </form>
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
