9eb734d3939e6e5a9a17187eefcf7815002e5282
Rajvardhan <rajthakur3619@gmail.com>
2020-07-17 11:08:41 +0530

    Styling the messages for current user

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
       {
         messages.map(message => (
-          <Message username={message.username} text={message.text} />
+          <Message username={username} message={message} />
         ))
       }
     </div>
